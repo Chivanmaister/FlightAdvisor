d41a4efc10de7733bca71b97ccc29de2be1d1ca2
Ivan Marovic <ivanceinwow@gmail.com>
2024-03-26 15:50:25 +0100

    Update exceptions with serial id

--- src/main/java/com/ivanm/flightadvisor/exception/ApplicationException.java
+++ src/main/java/com/ivanm/flightadvisor/exception/ApplicationException.java
@@ -1,6 +1,10 @@
 package com.ivanm.flightadvisor.exception;
 
+import java.io.Serial;
+
 public class ApplicationException extends RuntimeException {
+
+  @Serial private static final long serialVersionUID = -6095755989120915099L;
 
   public ApplicationException(String message) {
     super(message);
