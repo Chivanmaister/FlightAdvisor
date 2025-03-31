5a5ab07cb7ba5d0b1eba687468c3e2d78db88a9d
Ivan Marovic <ivanceinwow@gmail.com>
2025-03-31 16:30:49 +0200

    Code clean-up

--- src/main/java/com/ivanm/flightadvisor/service/RouteService.java
+++ src/main/java/com/ivanm/flightadvisor/service/RouteService.java
@@ -16,7 +16,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
